32bbf1ab3ef136de15053281915786f8f3129928
cachehash <cachehash@github.com>
2018-03-23 16:25:02 -0500

    switching to pc = offset instead of index

--- interp.c
+++ interp.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "main.h"
 
 typedef struct inst_t {
@@ -13,34 +14,40 @@ int pc;
 void grow(int amt) {
 	static int prev = 0;
 	if (amt > prev) {
-		imem = realloc(imem, amt*sizeof(*imem));
+		imem = realloc(imem, amt);
 		prev = amt;
 	}
+}
+void push(inst_t* i) {
+	int sz = sizeof(*imem);
+	grow(pc+sz);
+	char* buff = (void*) imem;
+	memcpy(buff+pc, i, sz);
+	pc += sz;
 }
 
 #define I_ZERO 0x80
 void mkInsts(Node* n) {
+	inst_t inst;
 	if (n == NULL) {
 		return;
 	}
 	switch (n->type) {
 	case LOOP: {
-		int dst = pc;
-		grow(pc+1);
-		imem[pc].type = n->type;
-		imem[pc].imm = 1;
+		int dst = pc/sizeof(*imem);
+		inst.type = n->type;
+		inst.imm = 1;
 
-		pc++;
+		push(&inst);
 
 		mkInsts(n->n[0].n);
 
-		grow(pc+1);
-		imem[dst].amt = pc;
+		imem[dst].amt = pc/sizeof(*imem);
 
-		imem[pc].type = n->type;
-		imem[pc].amt = dst;
-		imem[pc].imm = 0;
-		pc++;
+		inst.type = n->type;
+		inst.amt = dst;
+		inst.imm = 0;
+		push(&inst);
 	}
 	break;
 
@@ -51,56 +58,54 @@ void mkInsts(Node* n) {
 
 	case SUM:
 	case SHIFT:
-		grow(pc+1);
-		imem[pc].type = n->type;
-		imem[pc].amt = n->n[0].i;
-		pc++;
+		inst.type = n->type;
+		inst.amt = n->n[0].i;
+		push(&inst);
 	break;
 
 	case OUT:
 	case IN:
-		grow(pc+1);
-		imem[pc].type = n->type;
-		pc++;
+		inst.type = n->type;
+		push(&inst);
 	break;
 
 	case SET:
 		for (int z = 0; z < n->sz; z++) {
 			Point *p = n->n[z].p;
-			grow(pc+1);
-			imem[pc].type = n->type;
-			imem[pc].amt = p->x;
-			imem[pc].imm = p->y;
-			imem[pc].imm2 = p->z;
-			pc++;
+			inst.type = n->type;
+			inst.amt = p->x;
+			inst.imm = p->y;
+			inst.imm2 = p->z;
+			push(&inst);
 		}
-		grow(pc+1);
-		imem[pc].type = I_ZERO;
-		pc++;
+		inst.type = I_ZERO;
+		push(&inst);
 	break;
 	}
 }
 void interpret(size_t end, CELL_T * m) {
 	size_t i = 0;
 	I = &i;
+	pc /= sizeof(*imem);
 	for (size_t k = 0; k < end; k++) {
-		switch (imem[k].type) {
+		inst_t* inst = &imem[k];
+		switch (inst->type) {
 		case LOOP: {
 			int go = (m[i] != 0);
-			if (imem[k].imm) {
+			if (inst->imm) {
 				go = !go;
 			}
 			if (go) {
-				k = imem[k].amt-1;
+				k = inst->amt-1;
 				continue;
 			}
 		}
 		break;
 		case SUM:
-			m[i] += imem[k].amt;
+			m[i] += inst->amt;
 		break;
 		case SHIFT:
-			i += imem[k].amt;
+			i += inst->amt;
 		break;
 		case OUT:
 			putchar(m[i]);
@@ -110,9 +115,9 @@ void interpret(size_t end, CELL_T * m) {
 			m[i] = readChar(m[i]);
 		break;
 		case SET: {
-			int x = imem[k].amt;
-			int y = imem[k].imm;
-			int scale = imem[k].imm2;
+			int x = inst->amt;
+			int y = inst->imm;
+			int scale = inst->imm2;
 			m[i+x] += (y*m[i])/scale;
 		}
 		break;
